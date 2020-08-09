75ab6d30fcc6ad5cb2fb94fe10f704f922d4c7f3
Paul Ridgway <paul@ridgway.io>
2020-08-09 21:20:50 +0100

    basic working fns

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
 import simpleGit, { SimpleGit } from 'simple-git';
+import { inspect } from 'util';
+import { exec } from 'child_process';
 const git: SimpleGit = simpleGit();
 
-async function run() {
-  console.log(await git.log());
+function camelize(str: string): string {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index): string => {
+    return index === 0 ? word.toLowerCase() : word.toUpperCase();
+  }).replace(/\s+/g, '');
 }
 
-run();
+async function run(): Promise<void> {
+  console.log("Git Splat in", process.cwd);
+  const log = await git.log();
+  console.log(`Exporting ${log.total} commits...`);
+  log.all.forEach((l, i): void => {
+    console.log("Processing commit:", l.message);
+    const n = log.total - i;
+    const name = camelize(l.message.toLowerCase());
+    const cmd = `git archive --format=zip --output=${n}-${name}.zip ${l.hash}`;
+    exec(cmd, (error, stdout, stderr): void => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.warn(`stderr: ${stderr}`);
+        return;
+      }
+    });
+  });
+}
+
+run().catch((e): void => console.error(`Error: ${inspect(e)}`));
